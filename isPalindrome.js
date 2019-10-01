316db2310f9557f9b90ac49aab0d786513780d10
Natasha <8430654@gmail.com>
2019-10-01 22:41:53 +0300

    isPalindrom

---
+++ isPalindrome.js
@@ -0,0 +1,9 @@
+const isPalindrome = (str) => {
+    if (str === '') throw new Error ('Ошибка. Пустая строка');
+    strNew = String(str).toLowerCase();
+    if (strNew.length < 1) return true;
+    let strReverse = strNew.split('').reverse().join('');
+    if (strReverse == strNew) return true;
+    return false;
+};
+export default isPalindrome;
