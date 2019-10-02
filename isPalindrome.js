43dce2abde287e3033efea8957989d21f4a79ba3
Natasha <8430654@gmail.com>
2019-10-02 11:22:52 +0300

    Update

--- isPalindrome.js
+++ isPalindrome.js
@@ -3,7 +3,7 @@ const isPalindrome = (str) => {
     strNew = String(str).toLowerCase();
     if (strNew.length < 1) return true;
     let strReverse = strNew.split('').reverse().join('');
-    if (strReverse == strNew) return true;
+    if (strReverse === strNew) return true;
     return false;
 };
 export default isPalindrome;
