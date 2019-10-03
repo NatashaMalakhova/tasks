2d8ded0aad466ce08361af2e70d1ac8883824dfe
Natasha <8430654@gmail.com>
2019-10-03 22:31:21 +0300

    Update

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,10 +1,18 @@
 const isPalindrome = (str) => {
-    if (str === '') throw new Error ('Ошибка. Пустая строка');
-    strNew = String(str).toLowerCase();
-    if (strNew.length < 1) return true;
-    let strReverse = strNew.split('').reverse().join('');
-    if (strReverse === strNew) return true;
-    return false;
+    if (str === '') throw new Error ('There is no data. The empty string.');
+    if (str === undefined) throw new Error (' is not defined.');
+
+    str = String(str).toLowerCase().replace(/[/.,!?;_’%:\s/\-/\–/\—/]*/g, '');
+    
+    const result = (strNew) => {
+        let firstIndex = strNew[0];
+        let lastIndex = strNew[strNew.length - 1];
+        if (strNew.length <= 1) return true;
+        if (firstIndex !== lastIndex) return false;
+
+        return result(strNew.slice(1, -1));
+    };
+    return result(str); 
 };
 
 export default isPalindrome;
