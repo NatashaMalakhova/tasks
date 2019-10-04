e428c2a3c1217bf5f1863609591f54661e5eb5cf
Natasha <8430654@gmail.com>
2019-10-04 11:10:53 +0300

    Update Palindrom.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -4,13 +4,13 @@ const isPalindrome = (str) => {
 
     str = String(str).toLowerCase().replace(/[/.,!?;_’%:\s/\-/\–/\—/]*/g, '');
     
-    const result = (strNew) => {
-        let firstIndex = strNew[0];
-        let lastIndex = strNew[strNew.length - 1];
-        if (strNew.length <= 1) return true;
+    const result = (str) => {
+        let firstIndex = str[0];
+        let lastIndex = str[str.length - 1];
+        if (str.length <= 1) return true;
         if (firstIndex !== lastIndex) return false;
 
-        return result(strNew.slice(1, -1));
+        return result(str.slice(1, -1));
     };
     return result(str); 
 };
