d1fd47c1b56c4472a6c44bbf5bb1b15e0cb125fe
NatashaMalakhova <54099163+NatashaMalakhova@users.noreply.github.com>
2019-10-06 23:29:34 +0300

    Update apply.js

--- Tasks 3/apply.js
+++ Tasks 3/apply.js
@@ -1,12 +1,12 @@
-const apply = (iterationsOfFunction, nameOfFunction, argumentOfFunction) => {
-  if (typeof nameOfFunction !== 'function') throw new TypeError ('No function.');
-  if (argumentOfFunction === undefined) throw new Error ('Argument for function not passed.');
-  if (typeof iterationsOfFunction !== 'number') throw new TypeError ('The number of iterations must be of type "Number".');
-  if (iterationsOfFunction < 0) throw new Error ('The number of iterations is negative.');
-  if (Number.isInteger(iterationsOfFunction) === false) throw new Error ('The number of iterations cannot be a fractional number.');
+const apply = (iterations, fun, value) => {
+  if (typeof fun !== 'function') throw new TypeError ('No function.');
+  if (value === undefined) throw new Error ('Argument for function not passed.');
+  if (typeof iterations !== 'number') throw new TypeError ('The number of iterations must be of type "Number".');
+  if (iterations < 0) throw new Error ('The number of iterations is negative.');
+  if (Number.isInteger(iterations) === false) throw new Error ('The number of iterations cannot be a fractional number.');
 
-  if (iterationsOfFunction === 0) return argumentOfFunction;
-  return apply(iterationsOfFunction - 1, nameOfFunction, nameOfFunction(argumentOfFunction));
+  if (iterations === 0) return value;
+  return apply(iterations - 1, fun, fun(value));
 };
 
 export default apply;
